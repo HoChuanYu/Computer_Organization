7093d9b8cc37222c720b2804153877b1739088e1
HoChuanYu <44057809+HoChuanYu@users.noreply.github.com>
2022-03-16 21:12:09 +0800

    Add files via upload

--- LAB1/alu_top.v
+++ LAB1/alu_top.v
@@ -9,7 +9,7 @@ module alu_top(
                cin,        //1 bit carry in (input)
                operation,  //operation      (input)
                result,     //1 bit result   (output)
-               cout       //1 bit carry out(output)
+               cout,       //1 bit carry out(output)
                );
 
 input         src1;
@@ -23,43 +23,10 @@ input [2-1:0] operation;
 output        result;
 output        cout;
 
-reg           result1;
-reg           cout1;
-wire          carry;
-wire          sum;
-assign result = result1;
-assign cout = cout1;
-
-add add(
-    .A(src1),
-    .B(B_invert?!src2:src2),
-    .CIN(cin),
-    .COUT(carry),
-    .SUM(sum)
-);
+reg           result;
 
 always@( * )
-begin   
-    case(operation)
-        2'b00:begin//and, nor
-            result1=(A_invert?!src1:src1)&(B_invert?!src2:src2);
-            cout1=0;
-        end
-        2'b01:begin//or,nand
-            result1=(A_invert?!src1:src1)|(B_invert?!src2:src2);
-            cout1=0;
-        end
-        2'b10:begin//add sub
-            result1=sum;
-            cout1=carry;
-        end
-        2'b11:begin
-            result1=less;
-            cout1=carry;  
-        end
-    
-    endcase
-
+begin
 
 end
 
